a46cc3133601e76bd2db230fdedd72bb727bdeee
ArabellaSiachongco <aps9434@students.uc-bcf.edu.ph>
2025-02-26 19:47:45 +0800

    done with layout and css, todo: API

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Main.css';
+import { assets } from '../../assets/assets.js';
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt="user_icon" />
+            </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, User name</span></p>
+                    <p>How can i help yoiu today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful places to see upcoming places</p>
+                        <img src={assets.compass_icon} alt="compass_icon" />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summarizes this concept: urban planning</p>
+                        <img src={assets.bulb_icon} alt="bulb_icon" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstorm team bonding activities for our work retreat</p>
+                        <img src={assets.message_icon} alt="message_icon" />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} alt="code_icon" />
+                    </div>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a promt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="gallery_icon" />
+                            <img src={assets.mic_icon} alt="mic_icon" />
+                            <img src={assets.send_icon} alt="send_icon" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                    Gemeni may display inaccurate info, including about people, so double check its responses.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
